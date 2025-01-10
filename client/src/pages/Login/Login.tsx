e5396d7980deb7440f29c52b57695d14603864e2
CyrusWeir <cyrusweir@gmail.com>
2025-01-10 14:47:53 +0100

    des petits changements pour la page login

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -4,11 +4,15 @@ import "./Login.css";
 //apres discussion avec benjamin, nous avons decider d'utiliser la page de connexion comme page de creation de compte
 function Login() {
   return (
-    <main className="login-container">
-      <h2 className="login-subheading">Rejoignez la communauté Casse Croûte</h2>
-      {/* <img src="{`${import.meta.env.VITE_API_URL}/assets/images/favicon.png`}"alt="logo" /> */}
-      <CreateAccount />
-    </main>
+    <>
+      <main className="login-container">
+        <h2 className="login-subheading">
+          Rejoignez la communauté Casse Croûte
+        </h2>
+        {/* <img src="{`${import.meta.env.VITE_API_URL}/assets/images/favicon.png`}"alt="logo" /> */}
+        <CreateAccount />
+      </main>
+    </>
   );
 }
 
