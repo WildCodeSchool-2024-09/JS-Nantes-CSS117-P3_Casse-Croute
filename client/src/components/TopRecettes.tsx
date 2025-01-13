b8da16a36a680c6c940be836a8d8b58373377fb0
FarCodeFR <Timo-44@hotmail.com>
2025-01-13 16:05:05 +0100

    création du components TopRecette.tsx plus ajout d'une image test

---
+++ client/src/components/TopRecettes.tsx
@@ -0,0 +1,16 @@
+function TopRecettes() {
+  return (
+    <>
+      <section>
+        <h2>Les recettes les mieux notées ce mois-ci</h2>
+      </section>
+      <picture className="top-recette">
+        <section>
+          <h3>TOP 1</h3>
+        </section>
+        <img src="./assets/images/image-TOP1.jpg" alt="" />
+      </picture>
+    </>
+  );
+}
+export default TopRecettes;
