f9edcd79d7a9ec2b774375baeb853946e9f0c878
CyrusWeir <cyrusweir@gmail.com>
2025-01-22 10:42:56 +0100

    j'ai mis en place le logic dans le backend pour faire créer un compte, login, et verifier tous les utilisateurs

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -6,12 +6,12 @@ USE cassecroute_db;
 -- Table utilisateur
 CREATE TABLE utilisateur (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    pseudo VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
+    pseudo VARCHAR(50),
+    email VARCHAR(255) UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
-    date_inscription DATE NOT NULL,
+    date_inscription DATE,
     photo_profil VARCHAR(255),
-    est_admin BOOLEAN NOT NULL DEFAULT FALSE
+    est_admin BOOLEAN DEFAULT FALSE
 );
 
 -- Table type_recette
--- server/src/modules/user/userRepository.ts
+++ server/src/modules/user/userRepository.ts
@@ -2,36 +2,64 @@ import databaseClient from "../../../database/client";
 
 import type { Result, Rows } from "../../../database/client";
 
-type User = {
-  id: number;
-  pseudo: string;
+export type User = {
+  id?: number;
+  pseudo?: string | null;
+  mot_de_passe: string;
   email: string;
-  date_inscription: string;
-  photo_profil: string;
-  profilePic: string;
-  est_admin: boolean;
+  date_inscription?: string | null;
+  photo_profil?: string | null;
+  est_admin?: boolean | null;
 };
 
 class userRepository {
   async read(id: number) {
-    // Execute the SQL SELECT query to retrieve a specific item by its ID
+    // Execute the SQL SELECT query to retrieve a specific user by ID
     const [rows] = await databaseClient.query<Rows>(
       "select * from item where id = ?",
       [id],
     );
 
-    // Return the first row of the result, which represents the item
+    // Return the first row of the result, which represents the user
     return rows[0] as User;
   }
 
   async readAll(where = {}) {
-    // Execute the SQL SELECT query to retrieve all items from the "item" table
+    // Execute the SQL SELECT query to retrieve all users from the "utilisateur" table
     const [rows] = await databaseClient.query<Rows>(
       "select * from utilisateur",
     );
 
-    // Return the array of items
+    // Return the array of users
     return rows as User[];
+  }
+
+  async create(user: User) {
+    // Execute the SQL INSERT query to create a new user
+    const [result] = await databaseClient.query<Result>(
+      "INSERT INTO utilisateur (pseudo, email, date_inscription, photo_profil, est_admin, mot_de_passe) values (?, ?, ?, ?, ?, ?)",
+      [
+        user.pseudo,
+        user.email,
+        user.date_inscription,
+        user.photo_profil,
+        user.est_admin,
+        user.mot_de_passe,
+      ],
+    );
+
+    // Return the ID of the newly created user
+    return result.insertId;
+  }
+  async getUserByEmail(email: string) {
+    // Execute the SQL SELECT query to retrieve a specific item by its ID
+    const [rows] = await databaseClient.query<Rows>(
+      "select * from utilisateur where email = ?",
+      [email],
+    );
+
+    // Return the first row of the result, which represents the item
+    return rows[0] as User;
   }
 }
 export default new userRepository();
