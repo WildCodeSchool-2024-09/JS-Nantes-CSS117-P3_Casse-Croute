9904d77c5c953aea8d8cff5bbf6764b44a8261d6
Largoet <prunet.t@gmail.com>
2025-01-22 10:28:20 +0100

    retrait des commentaires en recette.d.ts

--- server/src/modules/recette/recette.d.ts
+++ server/src/modules/recette/recette.d.ts
@@ -1,12 +1,12 @@
 export interface Recette {
-  id?: number; // ID unique (auto-généré lors de la création)
-  titre: string; // Titre de la recette
-  description: string; // Description courte
-  date_publication: string; // Date de publication
-  image_url: string; // URL de l'image
-  saison: "printemps" | "été" | "automne" | "hiver" | "toutes saisons" | null; // Enum pour la saison
-  type_id: number; // Clé étrangère vers type_recette
-  difficulte_id: number; // Clé étrangère vers difficulte
-  temps_id: number; // Clé étrangère vers temps_preparation
-  utilisateur_id: number; // Clé étrangère vers utilisateur
+  id?: number;
+  titre: string;
+  description: string;
+  date_publication: string;
+  image_url: string;
+  saison: "printemps" | "été" | "automne" | "hiver" | "toutes saisons" | null;
+  type_id: number;
+  difficulte_id: number;
+  temps_id: number;
+  utilisateur_id: number;
 }
