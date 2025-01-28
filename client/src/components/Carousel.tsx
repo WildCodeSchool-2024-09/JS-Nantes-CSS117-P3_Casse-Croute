54e1141282c077cdc0609830edf1091fbdc66636
FarCodeFR <Timo-44@hotmail.com>
2025-01-28 22:11:14 +0100

    carousel responsive

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -6,69 +6,43 @@ function Carousel() {
       </section>
       <section className="container-carousel">
         <ul className="carousel-items">
-          <li className="carousel-item">
-            <div id="point1" className="slide-item">
+          <li id="point1" className="carousel-item">
+            <div className="slide-item">
               <img
                 src="/assets/images/carousel-recipes/image-1-carousel.jpg"
                 alt="sauté de légumes"
               />
-              <section className="container-points">
-                <ul className="first-carousel">
-                  <a href="#point1">
-                    <li>.</li>
-                  </a>
-                  <a href="#point2">
-                    <li>.</li>
-                  </a>
-                  <a href="#point3">
-                    <li>.</li>
-                  </a>
-                </ul>
-              </section>
             </div>
           </li>
-          <li className="carousel-item">
-            <div id="point2" className="slide-item">
+          <li id="point2" className="carousel-item">
+            <div className="slide-item">
               <img
                 src="/assets/images/carousel-recipes/image-2-carousel.jpg"
                 alt="yaourt aux noix, framboise et mûre"
               />
-              <section className="container-points">
-                <ul className="second-carousel">
-                  <a href="#point1">
-                    <li>.</li>
-                  </a>
-                  <a href="#point2">
-                    <li>.</li>
-                  </a>
-                  <a href="#point3">
-                    <li>.</li>
-                  </a>
-                </ul>
-              </section>
             </div>
           </li>
-          <li className="carousel-item">
-            <div id="point3" className="slide-item">
+          <li id="point3" className="carousel-item">
+            <div className="slide-item">
               <img
                 src="/assets/images/carousel-recipes/image-3-carousel.jpg"
                 alt="Des ribs accompagnés de frites et sa sauce barbecue"
               />
-              <section className="container-points">
-                <ul className="third-carousel">
-                  <a href="#point1">
-                    <li>.</li>
-                  </a>
-                  <a href="#point2">
-                    <li>.</li>
-                  </a>
-                  <a href="#point3">
-                    <li>.</li>
-                  </a>
-                </ul>
-              </section>
             </div>
           </li>
+        </ul>
+      </section>
+      <section className="container-points">
+        <ul className="first-carousel">
+          <a href="#point1">
+            <li>.</li>
+          </a>
+          <a href="#point2">
+            <li>.</li>
+          </a>
+          <a href="#point3">
+            <li>.</li>
+          </a>
         </ul>
       </section>
     </>
