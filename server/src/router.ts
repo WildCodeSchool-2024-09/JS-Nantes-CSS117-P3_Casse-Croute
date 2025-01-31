ee11b5a3b7e44b197b56a36f1a2baef44d767c88
CyrusWeir <cyrusweir@gmail.com>
2025-01-31 12:19:28 +0100

    Upload marche. C'etait une erreur de chemin dans mon test.

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 
 const router = express.Router();
 
@@ -25,6 +26,22 @@ router.get("/api/users", userActions.browse);
 router.post("/api/users", userActions.hashPassword, userActions.add);
 //Login
 router.post("/api/users/login", authActions.login);
+// addition of a file - this allows an upload to be placed in the public folder, and is renamed, adding the date in miliseconds to the filename
+const storage = multer.diskStorage({
+  destination: "./public/assets/images",
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+//upload route
+router.post(
+  "/api/users/upload",
+  upload.single("file"),
+  userActions.imageUpload,
+); //'file' corresponds to the name of the input from the client side of the site
 
 /* ************************************************************************* */
 
