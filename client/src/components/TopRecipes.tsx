8177672e4ae04344ad764ed96f2a6cf317499b8f
FarCodeFR <Timo-44@hotmail.com>
2025-01-31 16:34:51 +0100

    fix suppression des fichier test.http et client.http avec rm et ajout d'un nouveau fichier test.http que j'ai mis dans gitignore plus fix renommage des nom Français en Anglais + renommage des TOP en minuscule

--- client/src/components/TopRecipes.tsx
+++ client/src/components/TopRecipes.tsx
@@ -3,7 +3,7 @@ import "../styles/top-recipes.css";
 const topCard = [
   {
     id: 1,
-    titre: "TOP1",
+    titre: "top1",
     top: "TOP 1",
     image:
       "https://images.unsplash.com/photo-1513185158878-8d8c2a2a3da3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -11,21 +11,21 @@ const topCard = [
   },
   {
     id: 2,
-    titre: "TOP2",
+    titre: "top2",
     top: "TOP 2",
     image: "./assets/images/top-recipes/image-TOP2.jpg",
     alt: "Patte à la bolognaise accompagnée de ses épices",
   },
   {
     id: 3,
-    titre: "TOP3",
+    titre: "top3",
     top: "TOP 3",
     image: "./assets/images/top-recipes/image-TOP3.jpg",
     alt: "Pav bhaji, à base de curry de légumes épicé épais servi avec un petit pain beurré moelleux",
   },
   {
     id: 4,
-    titre: "TOP4",
+    titre: "top4",
     top: "TOP 4",
     image:
       "https://media.istockphoto.com/id/547025850/fr/photo/soupe-%C3%A0-loignon-avec-pain-sec-et-fromage-vue-de-dessus.jpg?s=612x612&w=0&k=20&c=3Xkabw0JuWllPM9ldj223Y6CN5ewMDfYS9wh6lWdjWU=",
@@ -33,7 +33,7 @@ const topCard = [
   },
   {
     id: 5,
-    titre: "TOP5",
+    titre: "top5",
     top: "TOP 5",
     image: "./assets/images/top-recipes/image-TOP5.jpg",
     alt: "Tour de pancakes avec son sirop d'érable et ses fruits sur le dessus",
@@ -43,7 +43,7 @@ const topCard = [
 function TopRecipes() {
   return (
     <>
-      <section className="container-titre-accueil">
+      <section className="container-title-accueil">
         <h2>Les recettes les mieux notées ce mois-ci</h2>
       </section>
       <div className="top-recipes-container">
